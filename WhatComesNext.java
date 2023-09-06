0a80efce998d7e51dd944dd6d2b912b036e3f8e5
Thomas Vrielink <thomasvrielink92@gmail.com>
2023-09-06 12:50:54 +0200

    First 5 exercises added

---
+++ WhatComesNext.java
@@ -0,0 +1,10 @@
+public class WhatComesNext {
+	
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Hello, you!");
+		} else {
+			System.out.println("Hello, " + args[0] + "!");
+		}
+	}
+}
